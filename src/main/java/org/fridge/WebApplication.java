0de35f680e370ff73c59a6e92ac2ef957acfd551
HHR <i@huhaorui.com>
2021-02-26 22:04:50 +0800

    添加OpenAPI支持

--- src/main/java/org/fridge/WebApplication.java
+++ src/main/java/org/fridge/WebApplication.java
@@ -3,9 +3,11 @@ package org.fridge;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @ServletComponentScan
 @SpringBootApplication
+@EnableOpenApi
 public class WebApplication {
 
     public static void main(String[] args) {
