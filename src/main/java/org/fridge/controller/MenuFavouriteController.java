40d3fd2858557109c5f3dae75a4002ca614131f9
DreamFish12450 <45327920+DreamFish12450@users.noreply.github.com>
2021-03-12 21:06:46 +0800

    have some bug

---
+++ src/main/java/org/fridge/controller/MenuFavouriteController.java
@@ -0,0 +1,27 @@
+package org.fridge.controller;
+
+import org.fridge.model.Menu;
+import org.fridge.model.MenuFavourite;
+import org.fridge.model.common.ApiResponse;
+import org.fridge.service.MenuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@ResponseBody
+public class MenuFavouriteController {
+    MenuService menuService;
+    @Autowired
+    public void setMenuService(MenuService menuService) {
+        this.menuService = menuService;
+    }
+    @PostMapping(value = "/menu/select/favourite")
+    @ResponseBody
+    public List<MenuFavourite> selectFavouriteByUserId(int id){
+        return menuService.selectFavourite(id);
+    }
+}
--- src/main/java/org/fridge/service/MenuService.java
+++ src/main/java/org/fridge/service/MenuService.java
@@ -1,22 +1,31 @@
 package org.fridge.service;
 
+import org.fridge.mapper.MenuFavouriteMapper;
 import org.fridge.mapper.MenuMapper;
 import org.fridge.model.Menu;
+import org.fridge.model.MenuFavourite;
 import org.fridge.model.common.ApiResponse;
 import org.fridge.model.common.Responses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class MenuService {
     MenuMapper menuMapper;
+    MenuFavouriteMapper menuFavouriteMapper;
 
     @Autowired
     public void setMenuMapper(MenuMapper menuMapper) {
         this.menuMapper = menuMapper;
+    }
+
+    @Autowired
+    public void setMenuFavouriteMapper(MenuFavouriteMapper menuFavouriteMapper) {
+        this.menuFavouriteMapper = menuFavouriteMapper;
     }
 
     public ApiResponse<Menu> selectNullTagItem() {
@@ -32,7 +41,11 @@ public class MenuService {
         }
     }
 
-    public List<Menu> MenuQueryByName(String menu){
+    public List<MenuFavourite> selectFavourite(int userId) {
+        return ( menuFavouriteMapper.selectMenuFavouriteByUserId(userId));
+    }
+
+    public List<Menu> MenuQueryByName(String menu) {
         return menuMapper.selectMenuByName(menu);
     }
 }
